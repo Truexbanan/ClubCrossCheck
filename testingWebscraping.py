0467ab1d8c4c6f151bee43e960975140ce36953f
Banan Truex <banan@bu.edu>
2023-11-09 12:05:08 -0500

    changed js file added python hash map and json file
    changed the js file to add a json file with all the data and then added python code that uses the json file and makes a hash map with the data.

--- testingWebscraping.py
+++ testingWebscraping.py
@@ -1,38 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
+import json
 
-#rul is https://www.worldfootball.net/teams/liverpool-fc/10/ with liverppol-fc changing to anyother team
-def get_players(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'
-    }
+# Assuming the JSON data is saved in a file named 'teamPlayers.json'
+with open('teamPlayers.json', 'r') as file:
+    team_players = json.load(file)
 
-    response = requests.get(url, headers=headers)
-    # idk this what spenccer wrote print(response.content.decode())
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the HTML content
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Use the correct CSS selector to find all player names
-        # This selector targets "a" tags inside "td" tags with the class "hell" and "dunkel"
-        player_links = soup.select('a[href*="player_summary"]')
-        print(f"Number of player links found: {len(player_links)}")
-
-        # Extract and print the player names
-        for link in player_links:
-            print(link.get_text())
-    
-        # Check for a 'next' link and if found, call `get_players` recursively
-        next_link = soup.find('a', string='Next »')
-        if next_link and 'href' in next_link.attrs:
-            next_url = next_link.attrs['href']
-            print(f"Fetching next page: {next_url}")
-            get_players(next_url)
-    else:
-        print(f"Failed to retrieve content, status code: {response.status_code}")
-
-# Starting URL of the team's player list page
-start_url = 'https://www.worldfootball.net/teams/liverpool-fc/'
-get_players(start_url)
+for key, value in team_players.items():
+    print(f"{key}: {value}")
--- testingwebscraping.js
+++ testingwebscraping.js
@@ -1,23 +1,32 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs'); // Import the File System module
 
-(async() => {
-    let Browser = await puppeteer.launch({
-        headless: false,
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false, // Set to true if you don't need to see the browser GUI
     });
+    const page = await browser.newPage();
     
-    let pages = await Browser.pages();
-    let page = pages[0];
-    
-    await page.goto('https://www.worldfootball.net/teams/liverpool-fc/10/', {
-    waitUntil: 'domcontentloaded',
-    timeout: 60000 // Increases timeout to 60 seconds
-});
-    //scroll to bottom
-    
-    //get array of player
-    const elements = await page.$$eval('a[href*="player_summary"]', elements => {
-        return elements.map(element => element.innerText);
-    });
-    
-    console.log(elements);
+    const teams = ['arsenal-fc', 'chelsea-fc', 'manchester-united']; // Add more team names as needed
+    const teamPlayers = {};
+
+    for (const team of teams) {
+        const url = `https://www.worldfootball.net/teams/${team}/10/`;
+        await page.goto(url, {
+            waitUntil: 'domcontentloaded',
+            timeout: 60000
+        });
+
+        const players = await page.$$eval('a[href*="player_summary"]', elements => {
+            return elements.map(element => element.innerText);
+        });
+
+        teamPlayers[team] = players;
+    }
+
+    await browser.close();
+
+    // Write the teamPlayers object to a JSON file
+    fs.writeFileSync('teamPlayers.json', JSON.stringify(teamPlayers, null, 2), 'utf-8');
+    console.log('The teamPlayers.json file has been saved!');
 })();
